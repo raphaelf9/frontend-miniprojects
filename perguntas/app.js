3cbc1ab9cb750d0ffacb45db7d8cf6ce60462041
Raphael Faria <raphaelf9@outlook.com>
2023-04-21 12:43:21 -0300

    Perguntas frequentes

---
+++ perguntas/app.js
@@ -0,0 +1,12 @@
+ //percorrendo o DOM
+const btns = document.querySelectorAll('.question-btn')
+
+btns.forEach((btn)=>{
+  btn.addEventListener('click',(e)=>{
+    const question = e.currentTarget.parentElement.parentElement
+    question.classList.toggle('show-text')
+    
+  })
+})
+
+
